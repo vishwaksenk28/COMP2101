2e39b97793565de294691b284b38667d8e3c6d49
vishwaksenk28 <vishwaksenk28@gmail.com>
2020-01-23 14:58:16 -0800

    edited tar component

---
+++ bash/getpics.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# this script puts some picture files into a Pictures directory in our home directory
+# it does some error testing
+# it summarizes the Pictures directory when it is done
+#
+
+# Task 1: Improve this script to also retrieve and install the files kept in the https://zonzorp.net/pics.tgz tarfile
+#   - use the same kind of testing that is already in the script to only download the tarfile if you don't already have it and  to make sure the download and extraction commands work, then delete the local copy of the tarfile if the extraction was successful
+
+# make a Pictures directory if we don't have one - assumes we have a home directory
+test -d ~/Pictures || mkdir ~/Pictures
+
+# download a zipfile of pictures to our Pictures directory if it isn't already there - assumes you are online
+test -f ~/Pictures/pics.zip || wget -q -O ~/Pictures/pics.zip http://zonzorp.net/pics.zip
+
+# unpack the downloaded zipfile if it is there, then delete the local copy of the zipfile
+test -f ~/Pictures/pics.zip && unzip -d ~/Pictures -o -q ~/Pictures/pics.zip && rm ~/Pictures/pics.zip
+
+# Make a report on what we have in the Pictures directory
+test -d ~/Pictures && cat <<EOF
+Found $(find ~/Pictures -type f|wc -l) files in the Pictures directory.
+The Pictures directory uses $(du -sh ~/Pictures|awk '{print $1}') space on the disk.
+EOF
+
+
+# Downlod pics.tgz file into ~/Pictures directory
+ test -f ~/Pictures/pics.tgz || wget -q -O ~/Pictures/pics.tgz http://zonzorp.net/pics.tgz
+
+ # Extract pics.tgz file into ~/Pictures directory then removing the file pics.tgz
+test -f ~/Pictures/pics.tgz && tar -xf ~/Pictures/pics.tgz -C ~/Pictures && rm ~/Pictures/pics.tgz
