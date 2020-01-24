63238e4aabf41b891e2686662c791b2415896cfc
vishwaksenk28 <vishwaksenk28@gmail.com>
2020-01-23 18:33:47 -0800

    work on it

---
+++ bash/welcome-message.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+# This script produces a dynamic welcome message
+# it should look like
+#   Welcome to planet hostname, title name!
+
+# Task 1: Use the variable $USER instead of the myname variable to get your name
+# Task 2: Dynamically generate the value for your hostname variable using the hostname command - e.g. $(hostname)
+# Task 3: Add the time and day of the week to the welcome message using the format shown below
+#   Use a format like this:
+#   It is weekday at HH:MM AM.
+# Task 4: Set the title using the day of the week
+#   e.g. On Monday it might be Optimist, Tuesday might be Realist, Wednesday might be Pessimist, etc.
+#   You will need multiple tests to set a title
+#   Invent your own titles, do not use the ones from this example
+
+###############
+# Variables   #
+###############
+
+title=$til
+myname=$USER
+hostname=$(hostname)
+
+time=$(date +"%r")
+day=$(date +"%a")
+
+if [[ $day = Mon ]];
+then til="Marvelous"
+elif [[ $day = Tue ]];
+ then til="Talented"
+elif [[ $day = Wed ]];
+ then til="Wonderous"
+elif [[ $day = Thu ]];
+ then til=Tremendous
+elif [[ $day = Fri ]];
+ then til="fabulous"
+elif [[ $day = Sat ]];
+ then til="Sophisticated"
+elif [[ $day = Sun ]];
+ then til="Super"
+fi
+  #statements
+
+###############
+# Main        #
+###############
+cat <<EOF
+
+Welcome to planet $hostname, "$title $myname!"
+It is $day at $time.
+
+EOF
